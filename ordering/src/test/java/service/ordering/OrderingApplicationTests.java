a5710da27fcfb43106be6bf4ad4606bb1ca4cec6
Jovan <jovan0042@gmail.com>
2020-12-27 18:01:32 +0100

    Inital commit

---
+++ ordering/src/test/java/service/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package service.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
