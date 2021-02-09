dc8ddfd89f6d67357a9d0897335d6f7b1f53fe0d
Jovan <jovan0042@gmail.com>
2021-02-09 15:32:27 +0100

    service-provider-admin-panel: custom colours

--- service-provider-admin-panel/src/app/app.component.ts
+++ service-provider-admin-panel/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import {Component, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material/sidenav';
+const ELEMENT_DATA = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-root',
@@ -10,7 +22,21 @@ export class AppComponent {
   title = 'service-provider-admin-panel';
   sidenavOpened = false;
   @ViewChild('sidenav', {static: true}) sidenav: MatSidenav;
+  dataSource: [{}, {}];
+  displayedColumns: string[] = ['name', 'weight', 'symbol', 'position'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  data = ELEMENT_DATA;
 
+  addColumn() {
+    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
+    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
+  }
+
+  removeColumn() {
+    if (this.columnsToDisplay.length) {
+      this.columnsToDisplay.pop();
+    }
+  }
   toggle(): void {
     const matDrawerToggleResultPromise = this.sidenav.toggle();
     this.sidenavOpened = !this.sidenavOpened;
